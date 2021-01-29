0daeba8ccf60d109be9b4b350b09e48719556912
dave vebber <davevebber@gmail.com>
2021-01-29 13:56:52 -0800

    food category&yelp price dropdowns now work and will return an array of 20 different restaurants

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,15 @@
+const submitBtn = document.querySelector('#submitBtn');
+let yelpCategory = document.querySelector('#food-category');
+let yelpPrice = document.querySelector('#yelp-price');
+let yelpDelivery = document.querySelector('#delivery');
+
 // fetch for YELP -- need to get search term to update according to the dropdown options
 // also need price point to update according to dropdown option as well -- 1=$ 2=$$ 3=$$$ 4=$$$$
-fetch("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=losangeles&term=greek&price=2", {
+function getYelpFetch() {
+    let foodSearch = localStorage.getItem('food-category');
+    let yelpPrice = localStorage.getItem('yelp-price');
+
+fetch("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=losangeles&term=" + foodSearch + "&price=" + yelpPrice, {
     "method": "GET",
     "headers": {
         "Content-Type": "application/x-www-form-urlencoded",
@@ -14,6 +23,7 @@ fetch("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/se
     .catch(err => {
         console.error(err);
     });
+}
 
 // fetch for The Movie Database -- need to get genre term to update according to dropdown options
 fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=068bcb78c00e7bd39492e93efa6cd1c2&language=en-US')
@@ -23,14 +33,6 @@ fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=068bcb78c00e7bd3949
     .then(function (response) {
         console.log(response)
     });
-
-
-const submitBtn = document.querySelector('#submitBtn');
-let yelpCategory = document.querySelector('#food-category');
-let yelpPrice = document.querySelector('#yelp-price');
-let yelpDelivery = document.querySelector('#delivery');
-
-
 
 function getFoodCategory() {
     localStorage.setItem('food-category', yelpCategory.value);    
@@ -44,7 +46,8 @@ function getDelivery() {
     localStorage.setItem('delivery', yelpDelivery.value);
 }
 
-
+// event listeners for submitBtn
 submitBtn.addEventListener('click', getFoodCategory);
 submitBtn.addEventListener('click', getYelpPrice);
 submitBtn.addEventListener('click', getDelivery);
+submitBtn.addEventListener('click', getYelpFetch);
